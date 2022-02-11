608f5de202ae4a5385aa30a9909d3cb26aaa434f
or man <Ormn1996@gmail.com>
2022-02-11 17:17:31 +0200

    add finisher

---
+++ finisher.py
@@ -0,0 +1,20 @@
+from tqdm import tqdm
+from zipfile import ZipFile
+import os
+
+
+def finish(output_path, output_zip_path):
+    create_output_zip(output_path, output_zip_path)
+
+
+def create_output_zip(output_path, output_zip_path):
+    print("starting to create archive of output dir")
+    with ZipFile(output_zip_path, 'w') as zipObj:
+        # Iterate over all the files in directory
+        for folderName, subfolders, filenames in os.walk(output_path):
+            for filename in tqdm(desc=f"Packing({folderName})", iterable=filenames, unit="Files"):
+                # create complete filepath of file in directory
+                filePath = os.path.join(folderName, filename)
+                # Add file to zip
+                zipObj.write(filePath, filePath)
+    print("finish creating archive")
