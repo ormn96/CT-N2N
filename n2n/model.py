f98d33d9c98199394b18ef92b0606974b5453295
or man <Ormn1996@gmail.com>
2022-03-10 11:11:14 +0200

    refactor

--- n2n/model.py
+++ n2n/model.py
@@ -7,32 +7,6 @@ from keras import backend as K
 import tensorflow as tf
 
 
-class L0Loss:
-    def __init__(self):
-        self.gamma = K.variable(2.)
-
-    def __call__(self):
-        def calc_loss(y_true, y_pred):
-            loss = K.pow(K.abs(y_true - y_pred) + 1e-8, self.gamma)
-            return loss
-        return calc_loss
-
-
-class UpdateAnnealingParameter(Callback):
-    def __init__(self, gamma, nb_epochs, verbose=0):
-        super(UpdateAnnealingParameter, self).__init__()
-        self.gamma = gamma
-        self.nb_epochs = nb_epochs
-        self.verbose = verbose
-
-    def on_epoch_begin(self, epoch, logs=None):
-        new_gamma = 2.0 * (self.nb_epochs - epoch) / self.nb_epochs
-        K.set_value(self.gamma, new_gamma)
-
-        if self.verbose > 0:
-            print('\nEpoch %05d: UpdateAnnealingParameter reducing gamma to %s.' % (epoch + 1, new_gamma))
-
-
 def tf_log10(x):
     numerator = tf.math.log(x)
     denominator = tf.math.log(tf.constant(10, dtype=numerator.dtype))
@@ -43,44 +17,6 @@ def PSNR(y_true, y_pred):
     max_pixel = 255.0
     y_pred = K.clip(y_pred, 0.0, 255.0)
     return 10.0 * tf_log10((max_pixel ** 2) / (K.mean(K.square(y_pred - y_true))))
-
-
-def get_model(model_name="srresnet"):
-    if model_name == "srresnet":
-        return get_srresnet_model()
-    elif model_name == "unet":
-        return get_unet_model(out_ch=3)
-    else:
-        raise ValueError("model_name should be 'srresnet'or 'unet'")
-
-
-# SRResNet
-def get_srresnet_model(input_channel_num=3, feature_dim=64, resunit_num=16):
-    def _residual_block(inputs):
-        x = Conv2D(feature_dim, (3, 3), padding="same", kernel_initializer="he_normal")(inputs)
-        x = BatchNormalization()(x)
-        x = PReLU(shared_axes=[1, 2])(x)
-        x = Conv2D(feature_dim, (3, 3), padding="same", kernel_initializer="he_normal")(x)
-        x = BatchNormalization()(x)
-        m = Add()([x, inputs])
-
-        return m
-
-    inputs = Input(shape=(None, None, input_channel_num))
-    x = Conv2D(feature_dim, (3, 3), padding="same", kernel_initializer="he_normal")(inputs)
-    x = PReLU(shared_axes=[1, 2])(x)
-    x0 = x
-
-    for i in range(resunit_num):
-        x = _residual_block(x)
-
-    x = Conv2D(feature_dim, (3, 3), padding="same", kernel_initializer="he_normal")(x)
-    x = BatchNormalization()(x)
-    x = Add()([x, x0])
-    x = Conv2D(input_channel_num, (3, 3), padding="same", kernel_initializer="he_normal")(x)
-    model = Model(inputs=inputs, outputs=x)
-
-    return model
 
 
 # UNet: code from https://github.com/pietz/unet-keras
@@ -121,8 +57,7 @@ def get_unet_model(input_channel_num=1, out_ch=1, start_ch=64, depth=4, inc_rate
 
 
 def main():
-    # model = get_model()
-    model = get_model("unet")
+    model = get_unet_model()
     model.summary()
 
 
--- n2n/train.py
+++ n2n/train.py
@@ -3,8 +3,8 @@ import numpy as np
 from pathlib import Path
 from keras.callbacks import LearningRateScheduler, ModelCheckpoint
 from tensorflow.keras.optimizers import Adam
-from model import get_model, PSNR, L0Loss, UpdateAnnealingParameter
-from generator import NoisyImageGenerator, ValGenerator
+from model import get_unet_model, PSNR
+from tensorflow.keras.losses import Huber
 from noise_model import get_noise_model
 import dataset
 
@@ -35,6 +35,8 @@ def get_args(input_args):
                         help="training patch size")
     parser.add_argument("--batch_size", type=int, default=16,
                         help="batch size")
+    parser.add_argument("--network_depth", type=int, default=4,
+                        help="encoder-decoder network depth")
     parser.add_argument("--nb_epochs", type=int, default=100,
                         help="number of epochs")
     parser.add_argument("--lr", type=float, default=0.01,
@@ -42,19 +44,13 @@ def get_args(input_args):
     parser.add_argument("--steps", type=int, default=None,
                         help="steps per epoch")
     parser.add_argument("--loss", type=str, default="mse",
-                        help="loss; mse', 'mae', or 'l0' is expected")
+                        help="loss; mse', 'mae', or 'huber' is expected")
+    parser.add_argument("--huber_loss_delta", type=float, default=1.5,
+                        help="huber loss delta parameter")
     parser.add_argument("--weight", type=str, default=None,
                         help="weight file for restart")
     parser.add_argument("--output_path", type=str, default="checkpoints",
                         help="checkpoint dir")
-    parser.add_argument("--source_noise_model", type=str, default="gaussian,0,50",
-                        help="noise model for source images")
-    parser.add_argument("--target_noise_model", type=str, default="gaussian,0,50",
-                        help="noise model for target images")
-    parser.add_argument("--val_noise_model", type=str, default="gaussian,25,25",
-                        help="noise model for validation source images")
-    parser.add_argument("--model", type=str, default="srresnet",
-                        help="model architecture ('srresnet' or 'unet')")
     args = parser.parse_args(input_args)
 
     return args
@@ -70,8 +66,9 @@ def main(*input_args):
     lr = args.lr
     steps = args.steps
     loss_type = args.loss
-    output_path = Path(__file__).resolve().parent.joinpath(args.output_path)
-    model = get_model(args.model)
+    output_path = args.output_path
+    net_depth = args.network_depth
+    model = get_unet_model(depth=net_depth)
 
     if args.weight is not None:
         model.load_weights(args.weight)
@@ -79,20 +76,10 @@ def main(*input_args):
     opt = Adam(lr=lr)
     callbacks = []
 
-    if loss_type == "l0":
-        l0 = L0Loss()
-        callbacks.append(UpdateAnnealingParameter(l0.gamma, nb_epochs, verbose=1))
-        loss_type = l0()
+    if loss_type == "huber":
+        loss_type = Huber(args.huber_loss_delta)
 
     model.compile(optimizer=opt, loss=loss_type, metrics=[PSNR])
-    #
-    source_noise_model = get_noise_model(args.source_noise_model)
-    target_noise_model = get_noise_model(args.target_noise_model)
-    val_noise_model = get_noise_model(args.val_noise_model)
-    generator = NoisyImageGenerator(image_dir, source_noise_model, target_noise_model, batch_size=batch_size,
-                                    image_size=image_size)
-    val_generator = ValGenerator(test_dir, val_noise_model)
-    #
     ##
     train_ds = dataset.create_train_dataset(image_dir,batch_size=batch_size)
     val_ds = dataset.create_val_dataset(test_dir, batch_size=batch_size)
@@ -112,12 +99,6 @@ def main(*input_args):
         validation_data=val_ds,
         steps_per_epoch=steps,
     )
-    # hist = model.fit_generator(generator=generator,
-    #                            steps_per_epoch=steps,
-    #                            epochs=nb_epochs,
-    #                            validation_data=val_generator,
-    #                            verbose=1,
-    #                            callbacks=callbacks)
 
     np.savez(str(output_path.joinpath("history.npz")), history=hist.history)
 
