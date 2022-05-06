6f05d7ce985afc99bf03b9f69927a76525612baa
or man <Ormn1996@gmail.com>
2022-05-06 20:25:32 +0300

    negative values

--- n2n/sqrtMSE.py
+++ n2n/sqrtMSE.py
@@ -5,4 +5,4 @@ import tensorflow as tf
 class SqrtMSE(MeanSquaredError):
 
     def call(self, y_true, y_pred):
-        return tf.math.sqrt(super(SqrtMSE, self).call(y_true, y_pred))
+        return tf.math.sqrt(tf.nn.relu(super(SqrtMSE, self).call(y_true, y_pred)))
