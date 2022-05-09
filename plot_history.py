7f530e044138ef830ca2a569f54a0b5bd5fd2aa4
or man <Ormn1996@gmail.com>
2022-05-09 19:47:45 +0300

    change plotting order

--- plot_history.py
+++ plot_history.py
@@ -1,12 +1,14 @@
 from pathlib import Path
 import numpy as np
 import matplotlib.pyplot as plt
+from typing import List
+
 from util.CsvReader import CsvReader
 
 
-def main(input_path,output_path,output_name, npz_hist: dict, csv_hist: dict):
-    npz_input_paths = [(Path(input_path).joinpath(h, "history.npz"), n) for n, h in npz_hist.items()]
-    csv_input_paths = [(Path(input_path).joinpath(h, "history_watched.csv"), n) for n, h in csv_hist.items()]
+def main(input_path, output_path, output_name, npz_hist: List[tuple], csv_hist: List[tuple]):
+    npz_input_paths = [(Path(input_path).joinpath(h, "history.npz"), n) for n, h in npz_hist]
+    csv_input_paths = [(Path(input_path).joinpath(h, "history_watched.csv"), n) for n, h in csv_hist]
     datum = [(np.array(np.load(str(input_path), allow_pickle=True)["history"], ndmin=1)[0], n)
              for input_path, n in npz_input_paths]
     datum.extend([(CsvReader(input_path).to_dict(), n) for input_path, n in csv_input_paths])
@@ -23,4 +25,4 @@ def main(input_path,output_path,output_name, npz_hist: dict, csv_hist: dict):
 
 
 if __name__ == '__main__':
-    main("","","", dict(), {'test': "."})
+    main("","","", [], [('test', ".")])
