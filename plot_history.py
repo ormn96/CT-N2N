6c17a2d879800717a3f06e1087c01c7fea30e948
אור מן <ormn1996@gmail.com>
2022-05-09 11:25:06 +0300

    change plotter to dict

--- plot_history.py
+++ plot_history.py
@@ -3,17 +3,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 from util.CsvReader import CsvReader
 
-def main(input_path,npz_hist,csv_hist):
-    npz_input_paths = [Path(input_path).joinpath(h,"history.npz") for h in npz_hist]
-    csv_input_paths = [Path(input_path).joinpath(h, "history_watched.csv") for h in csv_hist]
-    datum = [(np.array(np.load(str(input_path), allow_pickle=True)["history"], ndmin=1)[0], input_path.parent.name)
-             for input_path in npz_input_paths]
-    datum.extend([(CsvReader(input_path).to_dict(), input_path.parent.name) for input_path in csv_input_paths])
+
+def main(input_path, npz_hist: dict, csv_hist: dict):
+    npz_input_paths = [(Path(input_path).joinpath(h, "history.npz"), n) for n, h in npz_hist.items()]
+    csv_input_paths = [(Path(input_path).joinpath(h, "history_watched.csv"), n) for n, h in csv_hist.items()]
+    datum = [(np.array(np.load(str(input_path), allow_pickle=True)["history"], ndmin=1)[0], n)
+             for input_path, n in npz_input_paths]
+    datum.extend([(CsvReader(input_path).to_dict(), n) for input_path, n in csv_input_paths])
     metrics = ["val_loss", "val_PSNR"]
 
     for metric in metrics:
         for data, setting_name in datum:
-            plt.plot(data[metric], label=setting_name)
+            plt.plot(list(map(float,data[metric])), label=setting_name)
         plt.xlabel("epochs")
         plt.ylabel(metric)
         plt.legend()
@@ -22,4 +23,4 @@ def main(input_path,npz_hist,csv_hist):
 
 
 if __name__ == '__main__':
-    main("",[],["."])
+    main("", dict(), {'test': "."})
