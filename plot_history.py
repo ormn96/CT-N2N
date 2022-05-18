e799aa7989d1a6dbcc57b533384b5b375be069ea
אור מן <ormn1996@gmail.com>
2022-05-18 09:05:06 +0300

    add loc and prop

--- plot_history.py
+++ plot_history.py
@@ -6,7 +6,8 @@ from typing import List
 from util.CsvReader import CsvReader
 
 
-def main(input_path, output_path, output_name, npz_hist: List[tuple], csv_hist: List[tuple]):
+def main(input_path, output_path, output_name, npz_hist: List[tuple], csv_hist: List[tuple], *, legend_prop=None,
+         legend_pos='best'):
     npz_input_paths = [(Path(input_path).joinpath(h, "history.npz"), n) for n, h in npz_hist]
     csv_input_paths = [(Path(input_path).joinpath(h, "history_watched.csv"), n) for n, h in csv_hist]
     datum = [(np.array(np.load(str(input_path), allow_pickle=True)["history"], ndmin=1)[0], n)
@@ -16,13 +17,13 @@ def main(input_path, output_path, output_name, npz_hist: List[tuple], csv_hist: 
 
     for metric in metrics:
         for data, setting_name in datum:
-            plt.plot(list(map(float,data[metric])), label=setting_name)
+            plt.plot(list(map(float, data[metric])), label=setting_name)
         plt.xlabel("epochs")
         plt.ylabel(metric)
-        plt.legend()
-        plt.savefig(Path(output_path).joinpath(output_name+'_'+metric + ".png"))
+        plt.legend(pos=legend_pos, prop=legend_prop)
+        plt.savefig(Path(output_path).joinpath(output_name + '_' + metric + ".png"))
         plt.cla()
 
 
 if __name__ == '__main__':
-    main("","","", [], [('test', ".")])
+    main("", "", "", [], [('test', ".")])
